ef78cef77fff8e4228e4949fdf123bbd84ad9e1f
Oliver Ni <oliver.ni@gmail.com>
2017-03-08 16:11:46 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+var today = new Date();
+var h = today.getHours();
+var m = today.getMinutes();
+var s = today.getSeconds();
+
+function pad(num) {
+  var s = "0" + num;
+  return s.substr(s.length-2);
+}
+
+function fullDate() {
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  return dayNames[today.getDay()] + ', ' + monthNames[today.getMonth()] + ' ' + today.getDate() + ', ' + today.getFullYear();
+}
+
+function updateTime() {
+  today = new Date();
+  h = today.getHours();
+  m = today.getMinutes();
+  s = today.getSeconds();
+
+  if ($("#h").text() != h) {
+    $("#h").fadeOut(timeFadeInt, function() {
+      $(this).text(pad(h)).fadeIn(timeFadeInt);
+    });
+  }
+  if ($("#m").text() != m) {
+    $("#m").fadeOut(timeFadeInt, function() {
+      $(this).text(pad(m)).fadeIn(timeFadeInt);
+    });
+  }
+  if ($("#s").text() != s) {
+    $("#s").fadeOut(timeFadeInt, function() {
+      $(this).text(pad(s)).fadeIn(timeFadeInt);
+    });
+  }
+  $("#date").text(fullDate())
+}
+
+$(function() {
+  $("body.fade").fadeIn(400).removeClass("fade");
+  $("#h").text(pad(h));
+  $("#m").text(pad(m));
+  $("#s").text(pad(s));
+  $("#date").text(fullDate())
+  setInterval(updateTime, 1000);
+});
