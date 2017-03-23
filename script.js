87eefc7c6a87c12a383227b74f9ba008889cf5bb
Oliver Ni <oliver.ni@gmail.com>
2017-03-23 10:39:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var font = 0;
 // Fonts
 
 function setFont(fontindex) {
-  chrome.storage.sync.set({"font": fontindex}, function() {});
+  chrome.storage.sync.set({font: fontindex}, function() {});
   document.body.style = "display: flex; font-family: '" + fonts[fontindex] + "';";
   font = fontindex;
 }
@@ -18,7 +18,7 @@ chrome.storage.sync.get("font", function(items) {
     setFont(0);
     font = 0;
   } else {
-    font = items[0]["font"]
+    font = items[0].font;
   }
 });
 
